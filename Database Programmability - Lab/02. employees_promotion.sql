c9e95d4e709ad518dd55d92af59c7af9b7a7925e
Kristina Braynova <123834461+krisibraynova01@users.noreply.github.com>
2023-09-29 10:51:22 +0300

    Add files via upload

---
+++ Database Programmability - Lab/02. employees_promotion.sql
@@ -0,0 +1,15 @@
+CREATE PROCEDURE sp_increase_salaries(department_name VARCHAR(50))
+LANGUAGE plpgsql
+AS $$
+BEGIN
+UPDATE employees AS e
+SET salary = salary * 1.05
+WHERE e.department_id = (
+SELECT department_id FROM departments
+	WHERE name = department_name
+);
+
+END; 
+$$;
+
+CALL sp_increase_salaries('Sales');
